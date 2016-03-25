6596ae88da3b954d1f10f649bf24c5bfaa2ed968
tiscomsci29 <dd.stitch55@gmail.com>
2016-03-26 02:15:04 +0700

    test sql

--- sql/project_webboard.sql
+++ sql/project_webboard.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 24, 2016 at 01:50 PM
--- Server version: 5.6.26
--- PHP Version: 5.6.12
+-- Generation Time: Mar 25, 2016 at 11:34 AM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 5.6.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -26,15 +26,70 @@ SET time_zone = "+00:00";
 -- Table structure for table `comments`
 --
 
-CREATE TABLE IF NOT EXISTS `comments` (
-  `id` int(5) unsigned NOT NULL,
+CREATE TABLE `comments` (
+  `id` int(5) UNSIGNED NOT NULL,
   `post_id` int(5) NOT NULL DEFAULT '0',
   `comment_id` int(5) NOT NULL DEFAULT '0',
   `details` text COLLATE utf8_unicode_ci NOT NULL,
-  `datetime` datetime NOT NULL,
   `comply` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `reply` int(3) NOT NULL DEFAULT '0'
+  `time` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `post_id`, `comment_id`, `details`, `comply`, `time`) VALUES
+(1, 1, 0, 'test comments', 'ความคิดเห็นที่ 1', '2016-03-25 15:10:55'),
+(2, 1, 0, 'test comments', 'ความคิดเห็นที่ 2', '2016-03-25 15:10:55'),
+(3, 1, 0, 'test comments', 'ความคิดเห็นที่ 3', '2016-03-25 15:10:55'),
+(4, 2, 0, 'test comments', 'ความคิดเห็นที่ 1', '2016-03-25 15:10:55'),
+(5, 2, 0, 'test comments', 'ความคิดเห็นที่ 2', '2016-03-25 15:10:55'),
+(6, 3, 0, 'test comments', 'ความคิดเห็นที่ 1', '2016-03-25 15:10:55'),
+(7, 3, 0, 'test comments', 'ความคิดเห็นที่ 2', '2016-03-25 15:10:55'),
+(8, 3, 0, 'test comments', 'ความคิดเห็นที่ 3', '2016-03-25 15:10:55'),
+(9, 3, 0, 'test comments', 'ความคิดเห็นที่ 4', '2016-03-25 15:10:55'),
+(10, 3, 0, 'test comments', 'ความคิดเห็นที่ 5', '2016-03-25 15:10:55'),
+(11, 4, 0, 'test comments', 'ความคิดเห็นที่ 1', '2016-03-25 15:10:55'),
+(12, 4, 0, 'test comments', 'ความคิดเห็นที่ 2', '2016-03-25 15:10:55'),
+(13, 5, 0, 'test comments', 'ความคิดเห็นที่ 1', '2016-03-25 15:10:55'),
+(14, 5, 0, 'test comments', 'ความคิดเห็นที่ 2', '2016-03-25 15:10:55'),
+(15, 1, 1, 'test reply 1', 'ตอบกลับ 1', '2016-03-25 15:21:50'),
+(16, 1, 1, 'test reply 1', 'ตอบกลับ 2', '2016-03-25 15:22:23'),
+(17, 1, 1, 'test reply 1', 'ตอบกลับ 3', '2016-03-25 15:22:23'),
+(18, 1, 2, 'test reply 2', 'ตอบกลับ 1', '2016-03-25 15:23:03'),
+(19, 1, 2, 'test reply 2', 'ตอบกลับ 2', '2016-03-25 15:23:03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment_reply`
+--
+
+CREATE TABLE `comment_reply` (
+  `comment_id` int(5) UNSIGNED NOT NULL,
+  `reply_num` int(3) UNSIGNED NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `comment_reply`
+--
+
+INSERT INTO `comment_reply` (`comment_id`, `reply_num`) VALUES
+(1, 3),
+(2, 2),
+(3, 0),
+(4, 0),
+(5, 0),
+(6, 0),
+(7, 0),
+(8, 0),
+(9, 0),
+(10, 0),
+(11, 0),
+(12, 0),
+(13, 0),
+(14, 0);
 
 -- --------------------------------------------------------
 
@@ -42,8 +97,8 @@ CREATE TABLE IF NOT EXISTS `comments` (
 -- Table structure for table `posts`
 --
 
-CREATE TABLE IF NOT EXISTS `posts` (
-  `id` int(5) unsigned NOT NULL,
+CREATE TABLE `posts` (
+  `id` int(5) UNSIGNED NOT NULL,
   `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `details` text COLLATE utf8_unicode_ci NOT NULL,
@@ -51,6 +106,17 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `time` datetime NOT NULL,
   `comment` int(3) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `name`, `title`, `details`, `picture`, `time`, `comment`) VALUES
+(1, 'name 1', 'title 1', 'details 1', 'picture 1', '2016-03-25 15:05:58', 3),
+(2, 'name 2', 'title 2', 'details 2', 'picture 2', '2016-03-25 15:05:58', 2),
+(3, 'name 3', 'title 3', 'details 3', 'picture 3', '2016-03-25 15:05:58', 5),
+(4, 'name 4', 'title 4', 'details 4', 'picture 4', '2016-03-25 15:05:58', 2),
+(5, 'name 5', 'title 5', 'details 5', 'picture 5', '2016-03-25 15:05:58', 2);
 
 --
 -- Indexes for dumped tables
@@ -61,6 +127,12 @@ CREATE TABLE IF NOT EXISTS `posts` (
 --
 ALTER TABLE `comments`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `comment_reply`
+--
+ALTER TABLE `comment_reply`
+  ADD PRIMARY KEY (`comment_id`);
 
 --
 -- Indexes for table `posts`
@@ -76,12 +148,12 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(5) unsigned NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(5) unsigned NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
